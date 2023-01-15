23dbf452a5c77fb00b964beb0e39a19d0d7d172f
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-15 22:35:13 +0300

    array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(record) {
+  let newRecord = record.find((member) => member.result === "W");
+  if (newRecord === undefined) return undefined;
+  else return newRecord.year;
+}
+superbowlWin(record);
